53750afd72f1dd3eb40ca557c6874dfd4c8ac9be
lasha-chipashvili-github <146829747+lasha-chipashvili-github@users.noreply.github.com>
2023-12-25 22:56:03 +0400

    Delete hw_24_1 directory

--- hw_24_1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import FilmListView, FilmDetailView, FilmCreateView
-
-urlpatterns = [
-    path('films/', FilmListView.as_view(), name='film_list'),
-    path('films/<int:pk>/', FilmDetailView.as_view(), name='film_detail'),
-    path('films/add/', FilmCreateView.as_view(), name='film_create'),
-]
